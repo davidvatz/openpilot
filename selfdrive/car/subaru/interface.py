e089a37aa1e8191f08178a9a2b338e9c64a93279
davidvatz <davidvatz@gmail.com>
2021-05-05 22:28:42 -0400

    Add 2020 impreza tuning

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -43,14 +43,17 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.0025, 0.1], [0.00025, 0.01]]
 
     if candidate == CAR.IMPREZA:
-      ret.mass = 1568. + STD_CARGO_KG
+      ret.mass = 1408. + STD_CARGO_KG
       ret.wheelbase = 2.67
       ret.centerToFront = ret.wheelbase * 0.5
-      ret.steerRatio = 15
-      ret.steerActuatorDelay = 0.4   # end-to-end angle controller
-      ret.lateralTuning.pid.kf = 0.00005
-      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2, 0.3], [0.02, 0.03]]
+      ret.steerRatio = 13
+      ret.steerActuatorDelay = 0.12  # end-to-end angle controller
+      ret.lateralTuning.pid.kf = 0.00007843996051470903
+#      ret.lateralTuning.pid.newKfTuned = True
+      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 14., 23.], [0., 14., 23.]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.01, 0.065, 0.11], [0.001, 0.015, 0.01]]
+#      ret.lateralTuning.pid.kdV = [0.7]
+
 
     if candidate == CAR.FORESTER:
       ret.mass = 1568. + STD_CARGO_KG
